918479af45b304556d29848bf9662485c210bbe1
Alex Wang <alexwang94834@gmail.com>
2026-01-27 12:53:58 -0500

    DEBUG: add missing LL constants

--- src/main/java/com/stuypulse/robot/constants/Cameras.java
+++ src/main/java/com/stuypulse/robot/constants/Cameras.java
@@ -9,10 +9,14 @@ import edu.wpi.first.math.util.Units;
 
 /** This interface stores information about each camera. */
 public interface Cameras {
-    
+
     public Camera[] LimelightCameras = new Camera[] {
-        //TODO: Set up camera pose
-        new Camera("limelight-turret", new Pose3d(Units.inchesToMeters(0), Units.inchesToMeters(0), Units.inchesToMeters(0), new Rotation3d(Units.degreesToRadians(0), Units.degreesToRadians(0), Units.degreesToRadians(0))), Settings.EnabledSubsystems.LIMELIGHT_TURRET)
+            // TODO: Set up camera pose
+            new Camera("limelight-turret",
+                    new Pose3d(Units.inchesToMeters(6.5), Units.inchesToMeters(-11.308), Units.inchesToMeters(7.910),
+                            new Rotation3d(Units.degreesToRadians(180), Units.degreesToRadians(35.6),
+                                    Units.degreesToRadians(0))),
+                    Settings.EnabledSubsystems.LIMELIGHT_TURRET)
     };
 
     public static class Camera {
