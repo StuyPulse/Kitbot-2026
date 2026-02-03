45df01b57f5d287747414cce12a105b902c65d2e
Apetrock24 <yunusgubrud@gmail.com>
2026-02-03 11:00:21 -0500

    ADD: basic shoot on move command and shot calculator (not finished)

---
+++ src/main/java/com/stuypulse/robot/commands/swerve/SwerveDriveMovmentAlignToHub.java
@@ -0,0 +1,142 @@
+package com.stuypulse.robot.commands.swerve;
+
+import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
+import com.ctre.phoenix6.sim.ChassisReference;
+import com.ctre.phoenix6.swerve.SwerveRequest;
+import com.stuypulse.robot.Robot;
+import com.stuypulse.robot.constants.Constants;
+import com.stuypulse.robot.constants.Field;
+import com.stuypulse.robot.constants.Gains;
+import com.stuypulse.robot.constants.Settings;
+import com.stuypulse.robot.constants.Settings.Driver.Drive;
+import com.stuypulse.robot.constants.Settings.Swerve;
+import com.stuypulse.robot.constants.Settings.Swerve.Alignment;
+import com.stuypulse.robot.constants.Settings.Swerve.Assist;
+import com.stuypulse.robot.subsystems.superstructure.Superstructure;
+import com.stuypulse.robot.subsystems.swerve.CommandSwerveDrivetrain;
+import com.stuypulse.robot.util.ShotCalculator;
+import com.stuypulse.stuylib.control.angle.AngleController;
+import com.stuypulse.stuylib.control.angle.feedback.AnglePIDController;
+import com.stuypulse.stuylib.input.gamepads.AutoGamepad;
+import com.stuypulse.stuylib.math.Angle;
+import com.stuypulse.stuylib.math.SLMath;
+import com.stuypulse.stuylib.math.Vector2D;
+import com.stuypulse.stuylib.streams.numbers.IStream;
+import com.stuypulse.stuylib.streams.numbers.filters.LowPassFilter;
+import com.stuypulse.stuylib.streams.vectors.VStream;
+import com.stuypulse.stuylib.streams.vectors.filters.VDeadZone;
+import com.stuypulse.stuylib.streams.vectors.filters.VLowPassFilter;
+import com.stuypulse.stuylib.streams.vectors.filters.VRateLimit;
+import com.stuypulse.stuylib.util.AngleVelocity;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+
+public class SwerveDriveMovmentAlignToHub extends Command {
+
+    private final CommandSwerveDrivetrain swerve;
+    private final SwerveRequest.FieldCentric drive;
+    private final Superstructure superstructure;
+    private final AutoGamepad gamepad;
+    private ChassisSpeeds prevfieldRelRobotSpeeds;
+    private ChassisSpeeds fieldRelRobotSpeeds;
+    private VStream speed;
+
+    private final AngleController controller;
+    private final IStream angleVelocity;
+
+    public SwerveDriveMovmentAlignToHub(AutoGamepad gamepad) {
+        this.gamepad = gamepad;
+        superstructure = Superstructure.getInstance();
+        prevfieldRelRobotSpeeds = new ChassisSpeeds();
+        fieldRelRobotSpeeds = new ChassisSpeeds();
+        swerve = CommandSwerveDrivetrain.getInstance();
+
+        drive = new SwerveRequest.FieldCentric()
+                .withDeadband(Settings.Swerve.Constraints.MAX_VELOCITY_M_PER_S * Settings.Driver.Drive.DEADBAND.get())
+                .withRotationalDeadband(
+                        Settings.Swerve.Constraints.MAX_ANGULAR_ACCEL_RAD_PER_S * Settings.Driver.Turn.DEADBAND.get())
+                .withDriveRequestType(DriveRequestType.OpenLoopVoltage);
+
+        controller = new AnglePIDController(Gains.Swerve.Motion.THETA.kP, Gains.Swerve.Motion.THETA.kI,
+                Gains.Swerve.Motion.THETA.kD)
+                .setOutputFilter(x -> -x);
+
+        AngleVelocity derivative = new AngleVelocity();
+
+        angleVelocity = IStream.create(() -> derivative.get(Angle.fromRotation2d(getTargetAngle())))
+                .filtered(
+                        new LowPassFilter(Assist.ANGLE_DERIV_RC),
+                        // make angleVelocity contribute less once distance is less than REDUCED_FF_DIST
+                        // so that angular velocity doesn't oscillate
+                        x -> x * Math.min(1, getDistanceToTarget() / Assist.REDUCED_FF_DIST),
+                        // new RateLimit(Settings.Swerve.MAX_ANGULAR_ACCEL),
+                        x -> SLMath.clamp(x, -Settings.Swerve.Constraints.MAX_ANGULAR_VEL_RAD_PER_S,
+                                Settings.Swerve.Constraints.MAX_ANGULAR_VEL_RAD_PER_S),
+                        x -> -x);
+
+        speed = VStream.create(this::getDriverInputAsVelocity)
+        .filtered(
+            new VDeadZone(Drive.DEADBAND),
+            x -> x.clamp(1),
+            x -> x.pow(Drive.POWER.get()),
+            x -> x.mul(Swerve.Constraints.MAX_VELOCITY_M_PER_S),
+            new VRateLimit(Swerve.Constraints.MAX_ACCEL_M_PER_S_SQUARED),
+            new VLowPassFilter(Drive.RC)
+        );
+
+        addRequirements(swerve);
+    }
+
+    private Rotation2d getTargetAngle() {
+        Pose2d currentPose = CommandSwerveDrivetrain.getInstance().getPose();
+        prevfieldRelRobotSpeeds = fieldRelRobotSpeeds;
+        fieldRelRobotSpeeds = ChassisSpeeds.fromRobotRelativeSpeeds(swerve.getChassisSpeeds(),
+                currentPose.getRotation());
+        Translation2d speakerPose = ShotCalculator
+                .solveShootOnTheFly(new Pose3d(currentPose),
+                        (Robot.isBlue()) ? new Pose3d(Field.hubCenter)
+                                : new Pose3d(Field.transformToOppositeAlliance(Field.hubCenter)),
+                        prevfieldRelRobotSpeeds, fieldRelRobotSpeeds, superstructure.getState().getMainWheelsTargetSpeed() / 60.0, 5, 0.01)
+                .estimateTargetPose().getTranslation().toTranslation2d();
+        return currentPose.getTranslation().minus(speakerPose).getAngle();
+    }
+
+    private double getDistanceToTarget() {
+        Translation2d currentPose = CommandSwerveDrivetrain.getInstance().getPose().getTranslation();
+        Translation2d speakerPose = Field.getAllianceHubPose().getTranslation();
+        return currentPose.getDistance(speakerPose);
+    }
+
+    protected double getAngleError() {
+        return controller.getError().getRotation2d().getDegrees();
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+
+    private Vector2D getDriverInputAsVelocity() {
+        return new Vector2D(gamepad.getLeftStick().y, -gamepad.getLeftStick().x);
+    }
+
+    @Override
+    public void execute() {
+        swerve.setControl(swerve.getFieldCentricSwerveRequest()
+            .withVelocityX(speed.get().x)
+            .withVelocityY(speed.get().y)
+            .withRotationalRate(-angleVelocity.getAsDouble())
+        );
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        swerve.drive(new Vector2D(new Translation2d()), 0);
+    }
+}
---
+++ src/main/java/com/stuypulse/robot/util/ShotCalculator.java
@@ -0,0 +1,139 @@
+
+// BORROWED CODE FROM 868!!! some stuffs added/edited like yaw calcumulations. gracias
+
+package com.stuypulse.robot.util;
+
+import com.stuypulse.robot.Robot;
+import com.stuypulse.robot.constants.Settings;
+
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public final class ShotCalculator {
+    public static final double g = 9.81; // gravity is not a number
+
+    public record ShotSolution(
+        Rotation2d launchPitchAngle,
+        double flightTimeSeconds) {
+    }
+
+    public static ShotSolution solveBallisticWithSpeed(
+        Pose3d shooterPose,
+        Pose3d targetPose,
+        double launchSpeed) {
+
+        Translation3d s = shooterPose.getTranslation();
+        Translation3d t = targetPose.getTranslation();
+
+        double dx = t.getX() - s.getX();
+        double dy = t.getY() - s.getY();
+        double dz = t.getZ() - s.getZ();
+
+        double d = Math.hypot(dx, dy);
+        if (d < 1e-9) {
+            throw new IllegalArgumentException("Horizontal distance too small");
+        }
+
+        double v2 = launchSpeed * launchSpeed;
+
+        double discriminant = v2 * v2 - g * (g * d * d + 2.0 * dz * v2);
+        if (discriminant < 0) {
+            return new ShotSolution(Rotation2d.kZero, 0);
+        }
+
+        // LOW-ARC solution (use + for high arc)
+        double tanTheta = (v2 - Math.sqrt(discriminant)) / (g * d);
+
+        double launchPitch = Math.atan(tanTheta);
+
+        double vHoriz = launchSpeed * Math.cos(launchPitch);
+        double time = d / vHoriz;
+
+        return new ShotSolution(Rotation2d.fromRadians(launchPitch), time);
+    }
+
+
+    public record AlignAngleSolution(
+        Rotation2d launchPitchAngle,
+        Rotation2d requiredYaw,
+        Pose3d estimateTargetPose) {
+    }
+
+    public static AlignAngleSolution solveShootOnTheFly(
+        Pose3d shooterPose,
+        Pose3d targetPose,
+        ChassisSpeeds prevFieldRelRobotVelocity,
+        ChassisSpeeds fieldRelRobotVelocity,
+        double targetSpeedRps,
+        int maxIterations,
+        double timeTolerance) {
+
+        ShotSolution sol = solveBallisticWithSpeed(
+            shooterPose,
+            targetPose,
+            targetSpeedRps);
+
+        
+        double t = sol.flightTimeSeconds();
+        
+        Pose3d effectiveTarget = targetPose;
+        Translation3d s = shooterPose.getTranslation();
+            
+        for (int i = 0; i < maxIterations; i++) {
+
+            double axMetersPerSecondSquared = (fieldRelRobotVelocity.vxMetersPerSecond - prevFieldRelRobotVelocity.vxMetersPerSecond) / Settings.DT;
+            double ayMetersPerSecondSquared = (fieldRelRobotVelocity.vyMetersPerSecond - prevFieldRelRobotVelocity.vyMetersPerSecond) / Settings.DT;
+
+            double dx = fieldRelRobotVelocity.vxMetersPerSecond * t
+            + 0.5 * axMetersPerSecondSquared * t * t;
+
+            double dy = fieldRelRobotVelocity.vyMetersPerSecond * t
+            + 0.5 * ayMetersPerSecondSquared * t * t;
+
+            effectiveTarget = new Pose3d(
+                targetPose.getX() - dx,
+                targetPose.getY() - dy,
+                targetPose.getZ(),
+                targetPose.getRotation());
+
+            
+            // SmartDashboard.putNumber("hdsr/targetPose x", targetPose.getX());
+            // SmartDashboard.putNumber("hdsr/targetPose y", targetPose.getY());
+
+            // SmartDashboard.putNumber("hdsr/effectiveTargetPose x", effectiveTarget.getX());
+            // SmartDashboard.putNumber("hdsr/effectiveTargetPose y", effectiveTarget.getY());
+
+            ShotSolution newSol = solveBallisticWithSpeed(
+                shooterPose,
+                effectiveTarget,
+                targetSpeedRps);
+
+            sol = newSol;
+            if (Math.abs(newSol.flightTimeSeconds() - t) < timeTolerance) {
+                break;
+            }
+
+            t = newSol.flightTimeSeconds();
+
+        }
+
+        
+        Translation3d et = effectiveTarget.getTranslation();
+
+        // all the poses we pass in are field relative, 
+        // so calculated yaw (turret angle) should be field relative as well... right
+
+        double yaw = Math.atan2( //atan2(dy, dx)
+            et.getY() - s.getY(),
+            et.getX() - s.getX() 
+        ); 
+
+        return new AlignAngleSolution(
+            sol.launchPitchAngle(),
+            Rotation2d.fromRadians(yaw),
+            effectiveTarget);
+    }
+}
