f078dca9a7d4863ba2054b64c2bad12fb1d94eba
Jophy Wang <jophyw2008@gmail.com>
2026-02-14 10:34:17 -0500

    ADD: Made this branch for climb alignment testing

--- src/main/java/com/stuypulse/robot/constants/Ports.java
+++ src/main/java/com/stuypulse/robot/constants/Ports.java
@@ -1,8 +1,3 @@
-/************************ PROJECT KITBOT ************************/
-/* Copyright (c) 2026 StuyPulse Robotics. All rights reserved.*/
-/* This work is licensed under the terms of the MIT license.  */
-/**************************************************************/
-
 package com.stuypulse.robot.constants;
 
 /** This file contains the different ports of motors, solenoids and sensors */
